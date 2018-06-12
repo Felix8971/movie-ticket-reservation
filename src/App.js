84d50f5a12029a4cefba0798480f8bec2b54865f
Felix Debon <felix8971@hotmail.com>
2018-06-12 16:51:27 +0200

    maj scss

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ export default connect((state) => state)(
       super();
       this.state = {
         movies: [],
-        selection: 0,
+        buySeatModal: 0,//{ open: false, id: null },
+        modalTransaction: { open: false, data: {} },
+        detailsMovie: { open: false, id: null },
       };
 
     }
@@ -21,25 +23,26 @@ export default connect((state) => state)(
       fetch('http://localhost:3001/movies')
         .then(function (resp) { return resp.json(); })
         .then(function (data) {
+          //this.props.dispatch({type:'MOVIE_UPDATE', movies:data})
           self.setState({ movies: data })
         });
-
+      setTimeout(() => { this.props.dispatch({ type: 'SEAT', id: 2 }) }, 2000);
     }
 
     render() {
-
+      console.log('this.state=', this.state);
       const cardList = this.state.movies.map(function (item, i) {
         return (
 
           <div key={i} className="item-1">
             <div className="card">
-              <div className='img' style={{ backgroundImage: 'url(images/' + item.image + ')' }}></div>
+              <div className="img" style={{ backgroundImage: 'url(images/' + item.image + ')' }}></div>
               <article>
-                <h1>{item.title}</h1>
+                <h1 className="title">{item.title}</h1>
                 <button className="button">Buy seat</button>
               </article>
               <div className="seat-btn-bg" >
-                <div className="set-btn-container">
+                <div className="seat-btn-container">
                   <button className="button">Normal seat: 1.99 USD </button>
                   <button className="button">Superior seat: 2.99 USD </button>
                   <button className="button">Sofa seat: 5.99 USD </button>
--- src/index.js
+++ src/index.js
@@ -4,26 +4,51 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
 
-
-const store = createStore(function(state, action) {
-  const _state = state == null ? {
-    x: 0,
-    y: '',
-  } : state;
+const store = createStore(function (state, action) {
+  const _state = state == undefined ? {} : state;
+  console.log('_state=', state);
 
   switch (action.type) {
-    case 'TYPE1':
-      return Object.assign({}, _state, {
-        donate: _state.x + action.x,
-      });
-    case 'TYPE2':
-      return Object.assign({}, _state, {
-        y: action.y,
-      });
 
-    default: return _state;
+    case 'MOVIE_UPDATE':
+      return Object.assign({}, state, {//ne va modifier que la proprieté selectedMovieId dans la copy faite de l'objet state
+        movies: action.data,
+      });
+    case 'DETAILS':
+      return Object.assign({}, state, {//ne va modifier que la proprieté selectedMovieId dans la copy faite de l'objet state
+        detailsMovie: { open: true, id: action.id },
+      });
+    case 'SEAT':
+      const x = Object.assign({}, state, {
+        buySeatModal: 1,
+      });
+      console.log('x=', x);
+      return Object.assign({}, state, {
+        buySeatModal: 1,
+      });
+    default: return state;
   }
-},  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
+}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// const store = createStore(function(state, action) {
+//   const _state = state == null ? {
+//     x: 0,
+//     y: '',
+//   } : state;
+
+//   switch (action.type) {
+//     case 'TYPE1':
+//       return Object.assign({}, _state, {
+//         donate: _state.x + action.x,
+//       });
+//     case 'TYPE2':
+//       return Object.assign({}, _state, {
+//         y: action.y,
+//       });
+
+//     default: return _state;
+//   }
+// },  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
 
 
 render(
